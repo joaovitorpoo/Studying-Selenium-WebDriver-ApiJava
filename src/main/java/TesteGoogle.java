d937dce2d02928954b89899896bd7c635500c3f2
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-01-25 15:27:46 -0300

    Automating Other Browsers And Closing Software
    Opening page in Google Chrome and closing driver or page

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,14 +1,31 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.*;
 
 public class TesteGoogle {
+	
 	@Test
 	public void teste() {
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\estagio\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		// ----------------- DEFININDO DRIVER DO NAVEGADOR ------------------------------------------------------
+		
+		// FIREFOX 
+		// System.setProperty("webdriver.gecko.driver", "../Studying-Selenium-WebDriver-ApiJava/geckodriver.exe");
+		// WebDriver driver = new FirefoxDriver();
+		
+		// GOOGLE CHROME
+		System.setProperty("webdriver.chrome.driver", "../Studying-Selenium-WebDriver-ApiJava/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://www.google.com.br/");
-		Assert.assertEquals("Google", driver.getTitle());	
+		Assert.assertEquals("Google", driver.getTitle());
+		
+		// FECHA SO A ABA
+		// driver.close();
+		
+		// FECHA O DRIVE 
+		driver.quit();
 	}
+	
 }
