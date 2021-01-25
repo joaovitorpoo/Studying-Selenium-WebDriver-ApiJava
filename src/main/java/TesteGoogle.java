5b54f17aafe8f4e55c7effddf694e487714ccfe7
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-01-25 11:30:06 -0300

    Adding JUnit framework
    viewing Test Google errors with JUnit

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,12 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.*;
 
 public class TesteGoogle {
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\estagio\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver(); 
-		
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com.br/");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());	
 	}
 }
