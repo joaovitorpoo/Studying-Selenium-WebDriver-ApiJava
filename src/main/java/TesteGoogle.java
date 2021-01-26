fa2b1a23b43d01e5b7d8bc7aa454869da46b8241
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-01-26 09:20:39 -0300

    Window Position And Size

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,5 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.*;
@@ -11,21 +13,27 @@ public class TesteGoogle {
 		// ----------------- DEFININDO DRIVER DO NAVEGADOR ------------------------------------------------------
 		
 		// FIREFOX 
-		// System.setProperty("webdriver.gecko.driver", "../Studying-Selenium-WebDriver-ApiJava/geckodriver.exe");
-		// WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "../Studying-Selenium-WebDriver-ApiJava/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		
 		// GOOGLE CHROME
-		System.setProperty("webdriver.chrome.driver", "../Studying-Selenium-WebDriver-ApiJava/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		// System.setProperty("webdriver.chrome.driver", "../Studying-Selenium-WebDriver-ApiJava/chromedriver.exe");
+		// WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.google.com.br/");
 		Assert.assertEquals("Google", driver.getTitle());
+		
+		// POSICAO DA JANELA
+		// driver.manage().window().setPosition(new Point(500, 200));
+		
+		// TAMANHO DA JANELA
+		 driver.manage().window().setSize(new Dimension(400, 300));
 		
 		// FECHA SO A ABA
 		// driver.close();
 		
 		// FECHA O DRIVE 
-		driver.quit();
+		//driver.quit();
 	}
 	
 }
