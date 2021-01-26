6376dd214c222f12a8f24db428da364bcdbcbc7a
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-01-26 15:26:45 -0300

    Test TextField is TextArea

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,17 +1,28 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteCampoTreinamento {
 	
 	@Test
-	public void teste () {
+	public void testeTextField () {
 		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
-		
-		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Joao Vitor Pinto");
+		Assert.assertEquals("Joao Vitor Pinto", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		driver.quit();
 	}
 	
+	@Test 
+	public void testeTextArea() {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("testeeeeeeee\n\n\ntesteeeeeeeee\n\n\ntesteeeeeeeee");
+		Assert.assertEquals("testeeeeeeee\n\n\ntesteeeeeeeee\n\n\ntesteeeeeeeee", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		driver.quit();
+	}
 }
