4826c244aa907ba13878127c7ceed063b8b131d0
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-01-27 10:04:07 -0300

    Test Combo
    Select option
    Select multiple option
    Deselect option
    Check combo values

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,8 +1,12 @@
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 	
@@ -43,6 +47,77 @@ public class TesteCampoTreinamento {
 		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
 		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+		driver.quit();
+	}
+	
+	@Test 
+	public void testeCombo() {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
+		
+		// Criando objeto do combo 
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		
+		// Selecionando opção
+		// combo.selectByIndex(2);
+		// combo.selectByValue("2graucomp");
+		combo.selectByVisibleText("2o grau completo");
+		
+		Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+		
+		driver.quit();
+	}
+	
+	@Test 
+	public void testeComboVerificarOptions() {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
+		
+		// Criando objeto do combo 
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		List<WebElement> options = combo.getOptions();
+		Assert.assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for (WebElement option: options) {
+			if (option.getText().equals("2o grau completo")) {
+				encontrou = true;
+				break;
+			}
+		}
+		Assert.assertTrue(encontrou);
+		
+		driver.quit();
+	}
+	
+	@Test 
+	public void testeComboMultiplo() {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
+		
+		// Criando objeto do combo e selecionando opções
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		// Criando Lista com todas opções selecionadas e verificando tamanho 
+		List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectedOptions.size());
+		
+		// Deselecionando uma opção do combo 
+		combo.deselectByVisibleText("Corrida");
+		
+		// Criando Lista com todas opções selecionadas e verificando tamanho
+		allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectedOptions.size());
+		
 		driver.quit();
 	}
 }
