5c2556b89473b5d9f5d0edc723be753c895fab72
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-03 11:08:22 -0300

    switching of browser

--- src/main/java/br/ce/wcaquino/core/DriverFactory.java
+++ src/main/java/br/ce/wcaquino/core/DriverFactory.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino.core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -11,8 +12,16 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver(){
 		if(driver == null) {
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
-			driver = new FirefoxDriver();			
+			switch (Propriedades.browser) {
+				case FIREFOX: 
+					System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/main/resources/geckodriver.exe");
+					driver = new FirefoxDriver(); 
+					break;
+				case CHROME: 
+					System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/chromedriver.exe");
+					driver = new ChromeDriver();
+					break;
+			}
 		}
 		return driver;
 	}
--- src/main/java/br/ce/wcaquino/core/Propriedades.java
+++ src/main/java/br/ce/wcaquino/core/Propriedades.java
@@ -3,5 +3,10 @@ package br.ce.wcaquino.core;
 public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = false;
+	public static Browsers browser = Browsers.FIREFOX;
 	
+	public enum Browsers {
+		CHROME,
+		FIREFOX
+	}
 }
